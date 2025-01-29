176ae85cbbb4548695e25c8c9bc0aad1ba767ff8
yoas30 <yoas.rinatan30@gmail.com>
2025-01-29 23:32:04 +0700

    edit login controller

--- resources/views/include/navbarporto.blade.php
+++ resources/views/include/navbarporto.blade.php
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-2 px-0 px-lg-3 rounded" href="/">Home</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-2 px-0 px-lg-3 rounded" href="/login">Login Controller</a></li>
+                {{-- <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-2 px-0 px-lg-3 rounded" href="/login">Login Controller</a></li> --}}
             </ul>
         </div>  
     </div>
